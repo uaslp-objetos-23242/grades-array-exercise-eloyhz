f49664c4ff754b93f4f1fb30c402d1b656a33e63
Eloy Hernández <eloyhz@gmail.com>
2024-02-13 15:47:22 +0000

    Data input from main args

--- GradeBookDemo.java
+++ GradeBookDemo.java
@@ -3,7 +3,13 @@
 public class GradeBookDemo {
    public static void main(String[] args) {
       // array of student grades                                      
-      int[] gradesArray = {87, 68, 94, 100, 83, 78, 85, 91, 76, 87};
+      int[] gradesArray = new int[args.length];
+
+      int index = 0;
+      for (String s : args) {
+         int grade = Integer.parseInt(s);
+         gradesArray[index++] = grade;
+      }
       
       GradeBook myGradeBook = new GradeBook(
          "223401 - Object Oriented Technology", gradesArray);
